63814f08af0fb94ecac47c17f357334a88973749
Enzo Bogina <enzo.bogina@cci.com>
2025-09-12 11:55:24 +0200

    formulaire plus bootstrap

--- MVC/view/indexView.php
+++ MVC/view/indexView.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemple d'Application PHP avec  PDO, POO, DAO, MVC</title>
-    <!-- TODO ajouter bootstrap-->
-     <style>
-        input{
-            margin-top:5px;
-            margin-bottom:5px;
-        }
-        .right{
-            float: right;
-        }
-    </style>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exemple d'Application PHP avec  PDO, POO, DAO, MVC</title>
+        <!-- TODO ajouter bootstrap-->
+        <style>
+            input{
+                margin-top:5px;
+                margin-bottom:5px;
+            }
+            .right{
+                float: right;
+            }
+        </style>
+    </head>
+    <body>
+        <form action = "index.php?controller=articles&action=creer" method ="POST" class="col-lg-S">
+            <h3> Add Article </h3>
+            Nom: <input type="text" name = "nom" class="form-control">
+            Poid: <input type="text" name = "poid" class="form-control">
+            Prix: <input type="text" name = "prix" class="form-control">
+            <input type="submit" value="Send" class="btn btn-sucess"/>
+        </form>
+    </body>
 </html>
