0f5f102b9ac9770ed04789213b74a136d79e5f3e
Enzo Bogina <enzo.bogina@cci.com>
2025-09-12 11:48:53 +0200

    index view page d'accueil

---
+++ MVC/view/indexView.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemple d'Application PHP avec  PDO, POO, DAO, MVC</title>
+    <!-- TODO ajouter bootstrap-->
+     <style>
+        input{
+            margin-top:5px;
+            margin-bottom:5px;
+        }
+        .right{
+            float: right;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
