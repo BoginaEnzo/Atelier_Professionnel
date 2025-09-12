03d97b9b0a546ec5b4955a1ef84148efd1083e45
Enzo Bogina <enzo.bogina@cci.com>
2025-09-12 09:04:10 +0200

    index modifié

--- MVC/index.php
+++ MVC/index.php
@@ -1,3 +1,12 @@
 <?php
-    echo "Hello World";
+    require_once 'config/global.php';
+
+    //Gestion du controller par défaut
+    if(isset($_GET["controller"])){
+        $ccontrollerObj = loadController($GET["controller"]);
+        loadAction($controllerObj);
+    }
+    else{
+        $controllerObj = loadController(CONTROLLER_DEFAULT);
+    }
 ?>
