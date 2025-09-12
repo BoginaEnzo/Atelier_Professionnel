7ee052731ad1c07c5546cf5a20ba39d2e1e84183
Enzo Bogina <enzo.bogina@cci.com>
2025-09-12 08:50:46 +0200

    test sauvegarde

---
+++ MVC/Model/article.php
@@ -0,0 +1,16 @@
+    <?php
+        class Article{
+            private $table="article";
+            private $connexion;
+
+            private $art_id;
+            private $art_nom;
+            private $art_prix;
+            private $art_poid;
+
+
+            public function __construct($connexion){
+                
+            }
+        }
+    ?>
