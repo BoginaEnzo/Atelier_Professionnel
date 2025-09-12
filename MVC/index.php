f50b43845ba69517132189d32d5da15932cfeec4
Enzo Bogina <enzo.bogina@cci.com>
2025-09-12 09:16:43 +0200

    2 donctions ajouter LoadController charger le controller

--- MVC/index.php
+++ MVC/index.php
@@ -9,4 +9,30 @@
     else{
         $controllerObj = loadController(CONTROLLER_DEFAULT);
     }
+
+    function loadController($controller){
+        switch($controller){
+            case 'articles' :
+                $strFileController = 'controller/articlesController.php';
+                require_once $strFileController;
+                $controllerObj = new ArticlesController();
+            break;
+
+            default :
+                $strFileController = 'controller/articlesController.php';
+                require_once $strFileController;
+                $controllerObj = new ArticlesController();
+            break;
+        }
+
+        return $controllerObj;
+    }
+
+    function loadAction($controllerObj){
+        if(isset($_GET["action"])){
+            $controllerObj->run($_GET["action"]);
+        }else{
+            $controllerObj->run(ACTION_DEFAULT);
+        }
+    }
 ?>
