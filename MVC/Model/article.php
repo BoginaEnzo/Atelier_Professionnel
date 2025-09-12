4a204275b0c331cd763238744d41ca837982e329
Enzo Bogina <enzo.bogina@cci.com>
2025-09-12 11:09:04 +0200

    fonction upade, select, insert ajouter dans le code

--- MVC/Model/article.php
+++ MVC/Model/article.php
@@ -53,5 +53,41 @@
                 $this->connexion = null;
                 return $result;
             }
+
+            public function getById($id){
+                $query = $this->connexion->prepare("SELECT art_id, art_nom, art_prix, art_poid
+                FROM".$this->table."WHERE art_id = :id");
+                $query-> execute(array("id"=>"id"));
+                $result = $query->fetchObject();
+                $this->connexion = null;
+                return $result;
+            }
+
+            public function insert(){
+                $query = $this->connexion->prepare("INSERT INTO".$this->table."(art_nom, art_prix, art_poid) 
+                VALUES (:nom, :prix, :poid)");
+
+                $result = $query->execute(array(
+                    "nom"=>$this->art_nom,
+                    "prix"=>$this->art_prix,
+                    "poid"=>$this->art_poid,
+                ));
+                $this->connexion = null;
+                return $result;
+            }
+
+            public function update(){
+                $query = $this->connexion->prepare("UPDATE".$this->table."SET art_nom = :nom, art_prix = :prix, art_poid = :poid 
+                WHERE art_id = :id");
+
+                $result = $query->execute(array(
+                    "id" =>$this-> art_id,
+                    "nom"=>$this->art_nom,
+                    "prix"=>$this->art_prix,
+                    "poid"=>$this->art_poid,
+                ));
+                $this->connexion = null;
+                return $result;
+            }
         }
     ?>
