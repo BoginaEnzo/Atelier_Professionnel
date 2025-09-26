6cceb7f0726a5a27d0b8906af80b32bf35ac350c
Enzo Bogina <enzo.bogina@cci.com>
2025-09-26 10:57:39 +0200

    indexView.php programmé et commenté

--- MVC_Perso/view/indexView.php
+++ MVC_Perso/view/indexView.php
@@ -17,33 +17,38 @@
     </style>
 </head>
 <body>
-    <form action ="index.php?controller=articles&action=creer" 
+    // Form pour ajouter un animal
+    <form action ="index.php?controller=animals&action=creer" 
     method ="post" class="col-lg-5">
-        <h3>Add article</h3>
+        <h3>Add animal</h3>
         Nom: <input type="text" name="nom" class="form-control">
-        Poid: <input type="text" name="poid" class="form-control">
-        Prix: <input type="text" name="prix" class="form-control">
+        Espece: <input type="text" name="espece" class="form-control">
+        Age: <input type="text" name="age" class="form-control">
+        Race: <input type="text" name="race" class="form-control">
+        Photo: <input type="text" name="photo" class="form-control">
         <input type="submit" value="Send" class="btn btn-success"/>
     </form>
 
     <div class="col-lg-7">
-        <h3>Articles</h3>
+        <h3>Animals</h3>
         <hr/> 
     </div>
     
+    // Liste des animaux
     <section class="col-lg-7" style="height:400px;overflow-y:scroll;">
-        <?php foreach($data["article"] as $article) {?>
-            <?php echo $article["art_nom"]; ?> - 
-            <?php echo $article["art_prix"]; ?> - 
-            <?php echo $article["art_poid"]; ?> - 
+        <?php foreach($data["animal"] as $animal) {?>
+            <?php echo $animal["animal_nom"]; ?> - 
+            <?php echo $animal["animal_espece"]; ?> - 
+            <?php echo $animal["animal_age"]; ?> - 
+            <?php echo $animal["animal_race"]; ?> - 
             <div class="right">
                 <div class="btn btn-group">
-                <form action="index.php?controller=articles&action=delete" method="post">
-                    <input type="hidden" id="idDel" name="idDel" value="<?php echo $article['art_id']; ?>" />
+                <form action="index.php?controller=animals&action=delete" method="post">
+                    <input type="hidden" id="idDel" name="idDel" value="<?php echo $animal['animal_id']; ?>" />
                     <input type="submit" value="Supprimer" class="btn btn-danger"/>
                 </form>
-                
-                <a href="index.php?controller=articles&action=detail&id=<?php echo $article['art_id']; ?>" 
+
+                <a href="index.php?controller=animals&action=detail&id=<?php echo $animal['animal_id']; ?>" 
                 class="btn btn-info">detail</a>  
             </div>
         <hr/>
